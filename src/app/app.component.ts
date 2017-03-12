9922d361ddd61f8c6d07c37bebcdb45597904032
Malik <malikoma@amazon.com>
2017-03-11 19:31:22 -0500

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { OtherComponent } from './other/other.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: `
+    <h1>
+      {{title}}
+      <fa-other></fa-other>
+    </h1>
+`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
